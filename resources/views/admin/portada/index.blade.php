3667ff5a2f352e5f122e8af83bddf88d7cf503bc
hugo longas costa <hugolo3@gmail.com>
2024-02-07 18:37:46 +0100

    cover

--- resources/views/admin/portada/index.blade.php
+++ resources/views/admin/portada/index.blade.php
@@ -6,24 +6,63 @@
 @stop
 @section('content')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <div class="row">
-        {{ $covers }}
+    <div class="container">
         @for ($i = 0; $i < 3; $i++)
-            <div class="col" style="border:1px solid black;max-width:400px;height:500px;margin:5px">
-                <div>Titol:{{ $covers[$i]->title }}</div>
-                <div>Url:{{ $covers[$i]->url }}</div>
-                <div>Imatge:
-                    @if ($covers[$i]->img_url == null)
-                        <img class="img-fluid" style="width: 100%;" src="{{ asset('storage') }}/no_image.jpg" />
-                    @else
-                        <img class="img-fluid" style="width: 100px;"
-                            src="{{ asset('storage/media') . '/' . $covers[$i]->img_url }}" />
-                    @endif
+            @if ($i == 0)
+                <div class="row">
+                    <div class="col-12" style="border:1px solid black;margin:5px; width:100%;height:370px">
+                        <div>Titol:{{ $covers[$i]->title }}</div>
+                        <div>Url:{{ $covers[$i]->url }}</div>
+                        <div>Imatge:
+                            @if ($covers[$i]->img_url == null)
+                                <img class="img-fluid" style="width: 100%;" src="{{ asset('storage') }}/no_image.jpg" />
+                            @else
+                                <img class="img-fluid" style="width: 100px;"
+                                    src="{{ asset('storage/media') . '/' . $covers[$i]->img_url }}" />
+                            @endif
+                        </div>
+                        <a href="{{ route('admin.portada.edit', ['cover' => $covers[$i]]) }}"
+                            class="edit btn btn-primary btn.sm">Editar</a>
+                    </div>
                 </div>
-                <a href="{{route('admin.portada.edit', ['cover' => $covers[$i]])}}" class="edit btn btn-primary btn.sm">Editar</a>
-            </div>
-        @endfor
+            @endif
+            @if ($i == 1)
+                <div class="row">
+                    <div class="col-6" style="border:1px solid black;padding:5px">
+                        <div>Titol:{{ $covers[$i]->title }}</div>
+                        <div>Url:{{ $covers[$i]->url }}</div>
+                        <div>Imatge:
+                            @if ($covers[$i]->img_url == null)
+                                <img class="img-fluid" style="width: 100%;" src="{{ asset('storage') }}/no_image.jpg" />
+                            @else
+                                <img class="img-fluid" style="width: 100px;"
+                                    src="{{ asset('storage/media') . '/' . $covers[$i]->img_url }}" />
+                            @endif
+                        </div>
+                        <a href="{{ route('admin.portada.edit', ['cover' => $covers[$i]]) }}"
+                            class="edit btn btn-primary btn.sm">Editar</a>
+                    </div>
+            @endif
+            @if ($i == 2)
+                <div class="col-6" style="border:1px solid black;padding:5px">
+                    <div>Titol:{{ $covers[$i]->title }}</div>
+                    <div>Url:{{ $covers[$i]->url }}</div>
+                    <div>Imatge:
+                        @if ($covers[$i]->img_url == null)
+                            <img class="img-fluid" style="width: 100%;" src="{{ asset('storage') }}/no_image.jpg" />
+                        @else
+                            <img class="img-fluid" style="width: 100px;"
+                                src="{{ asset('storage/media') . '/' . $covers[$i]->img_url }}" />
+                        @endif
+                    </div>
+                    <a href="{{ route('admin.portada.edit', ['cover' => $covers[$i]]) }}"
+                        class="edit btn btn-primary btn.sm">Editar</a>
+                </div>
     </div>
+    @endif
+    @endfor
+    </div>
+
 @stop
 @push('scripts')
     <script></script>
--- resources/views/components/cover-item.blade.php
+++ resources/views/components/cover-item.blade.php
@@ -1,5 +1,5 @@
 <div class="cover-item {{ $cover->id==1 ? 'col-12':'col-6'}}">
-    <a href="{{ route('activitat', ['slug' => $cover->url]) }}">
+    <a href="{{ $cover->url }}">
         <div class="cover-poster">
             <img src="{{ asset('storage/media') . '/' . $cover->img_url }}" class="{{ $cover->id==1 ? 'poster-cover-top':'poster-cover'}}">
             <div class="cover-info">
